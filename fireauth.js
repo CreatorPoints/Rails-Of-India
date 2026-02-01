4f43a9fe9d1de8f4582d7f45505a5e62cfeee299
CreatorPoints <shrestangshudutta@gmail.com>
2026-02-01 21:06:55 +0530

    add firebase files

---
+++ fireauth.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDydOzvB5GwvSNAEcfyAFaD167ftlwW2U8",
+  authDomain: "rails-of-india-27a93.firebaseapp.com",
+  projectId: "rails-of-india-27a93",
+  storageBucket: "rails-of-india-27a93.firebasestorage.app",
+  messagingSenderId: "678135111193",
+  appId: "1:678135111193:web:4f8d3d052b1909edd25488",
+  measurementId: "G-PDPPRYL5N8"
+};
